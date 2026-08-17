c611bc33d334e1d24df0a9a2ff2cd98de7db886e
Firas Charfeddine <firas.charfeddine@tu-dortmund.de>
2026-08-18 00:52:41 +0100

    structure

--- word_List.py
+++ word_List.py
@@ -1,16 +1,57 @@
 import argparse
+import os
+from fileinput import close
+
 parser= argparse.ArgumentParser()
 parser.add_argument("-i","--input",required=True,help=" input txt")
 parser.add_argument("-o","--output",required=True,help="output txt")
 parser.add_argument("-L","--long",required=True,help="pwd min long ")
 parser.add_argument("-S","--security",required=False,help="security level for 1 to 3 ")
 arg = parser.parse_args()
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
 if arg.security != "1" and arg.security != "2" and arg.security != "3" and arg.security !=None :
     print ("security level ist for 1 to 3 ")
     exit()
 
-print(arg.input)
+if not os.path.exists(arg.input):
+    print("we dont fund die "+arg.input)
+    exit()
+
+if not os.path.exists(arg.output):
+    print("we dont fund die "+arg.output)
+    exit()
+
+"fehlt hier noch  die condition vonn die  L "
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+if arg.security==None :
+    arg.security="1"
+
+level=arg.security
+long= arg.long
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
 input= open(arg.input,"r")
-inhalt=input.readlines()
+keywort=input.readlines()
 input.close()
-print(inhalt)
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+wordlist=[]
+
+" hier fhler die logik von die reschnug vonn passeworte  Level 1   "
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" hier fhler die logik von die reschnug vonn passeworte  Level 2  "
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" hier fhler die logik von die reschnug vonn passeworte  Level 3   "
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+output=open(arg.output,"w")
+for i in wordlist :
+    output.write(i)
+output.close()
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
